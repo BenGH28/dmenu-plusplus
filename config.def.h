c15be365132458c60b9f41dd79f0d7a6bf2a301d
Ben Hunt <bigben.hunt@gmail.com>
2020-11-15 17:30:31 -0700

    Change the colours

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@ static int topbar = 1;                      /* -b  option; if 0, dmenu appears a
 static int incremental = 1;                 /* -r  option; if 1, outputs text each time a key is pressed */
 static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
 static int centered = 0;                    /* -c option; centers dmenu on screen */
-static int min_width = 500;                    /* minimum width when centered */
+static int min_width = 700;                    /* minimum width when centered */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
 	"JetBrainsMono Nerd Font:size=17"
@@ -13,15 +13,16 @@ static const char *fonts[] = {
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeSelHighlight] = { "#35ff99", "#005577" },
-	[SchemeNormHighlight] = { "#35ff99", "#222222" },
+	[SchemeNorm] = { "#bbbbbb", "#282a36" },
+	[SchemeSel] = { "#eeeeee", "#61afe0" },
+	[SchemeSelHighlight] = { "#35ff99", "#61afe0" },
+	[SchemeNormHighlight] = { "#35ff99", "#282a36" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
+
 /* -h option; minimum height of a menu line */
 static unsigned int lineheight = 0;
 static unsigned int min_lineheight = 8;

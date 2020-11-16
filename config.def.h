f4cbc550279d9050cc58c603028f0d167a1a4820
Ben Hunt <bigben.hunt@gmail.com>
2020-11-15 19:34:17 -0700

    Change the default width
    got rid of config.h since it is generated when you make dmenu
    so shouldn't have it in source control

--- config.def.h
+++ config.def.h
@@ -3,13 +3,15 @@
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 static int incremental = 1;                 /* -r  option; if 1, outputs text each time a key is pressed */
-static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
+static int fuzzy = 1;                      	/* -F  option; if 0, dmenu doesn't use fuzzy matching     */
 static int centered = 0;                    /* -c option; centers dmenu on screen */
-static int min_width = 700;                    /* minimum width when centered */
+static int min_width = 500;                 /* minimum width when centered */
+
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
 	"JetBrainsMono Nerd Font:size=17"
 };
+
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
